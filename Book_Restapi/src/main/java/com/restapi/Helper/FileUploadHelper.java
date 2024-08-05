ba8e98bf905027d89fb0eb4f17b30bce5104b282
Kashyap3699 <kashyap3627@gmail.com>
2024-08-05 17:22:37 +0530

    Dynamic path added

--- Book_Restapi/src/main/java/com/restapi/Helper/FileUploadHelper.java
+++ Book_Restapi/src/main/java/com/restapi/Helper/FileUploadHelper.java
@@ -2,24 +2,32 @@ package com.restapi.Helper;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
 
-	public final String UPLOAD_DIR = "D:\\Kashyap Folder\\Spring boot\\Book_Restapi\\src\\main\\resources\\static\\image";
+	//public final String UPLOAD_DIR = "D:\\Kashyap Folder\\Spring boot\\Book_Restapi\\src\\main\\resources\\static\\image";
+	public final String UPLOAD_DIR = new ClassPathResource("static/image/").getFile().getAbsolutePath();
+	
+	public FileUploadHelper() throws IOException {
+		
+	}
+
 	public boolean uploadFile(MultipartFile multipartFile) {
 		boolean f = false;
-		
+
 		try {
-			//read
+			// read
 //			InputStream inputStream = multipartFile.getInputStream();
 //			byte Data[] = new byte[inputStream.available()];
 //			inputStream.read(Data);
@@ -30,19 +38,19 @@ public class FileUploadHelper {
 //			fos.flush();
 //			fos.close();
 //			f=true;
-//			
-			
-			Files.copy(multipartFile.getInputStream(),Paths.get(UPLOAD_DIR + File.separator + 
-			multipartFile.getOriginalFilename()),StandardCopyOption.REPLACE_EXISTING);
-			
-			f =true;
-			
+
+			Files.copy(multipartFile.getInputStream(),
+					Paths.get(UPLOAD_DIR + File.separator + multipartFile.getOriginalFilename()),
+					StandardCopyOption.REPLACE_EXISTING);
+
+			f = true;
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
 		return f;
-		
+
 	}
 
 }
--- Book_Restapi/src/main/java/com/restapi/controller/FileUploadController.java
+++ Book_Restapi/src/main/java/com/restapi/controller/FileUploadController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.restapi.Helper.FileUploadHelper;
 
@@ -26,29 +27,28 @@ public class FileUploadController {
 		try {
 
 			if (multipartFile.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-						.body("Request must contain file");
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request must contain file");
 			}
 
 			if (!multipartFile.getContentType().equals("image/jpeg")) {
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-						.body("Only jpeg file allowed");
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only jpeg file allowed");
 			}
-			
+
 			// file upload code
 			boolean upFile = fileUploadHelper.uploadFile(multipartFile);
-			if(upFile) 
-			{
-				return ResponseEntity.ok("File uploaded Successfully");
+			if (upFile) {
+				//return ResponseEntity.ok("File uploaded Successfully");
+				
+				//Here we return full url of the image
+				//ServletUriComponentsBuilder.fromCurrentContextPath() - It will return localhost:3699
+				return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(multipartFile.getOriginalFilename()).toUriString());
 			}
-			
-		} catch (Exception e) 
-		{
+
+		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-				.body("Something went wrong");
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
 	}
 
 }
